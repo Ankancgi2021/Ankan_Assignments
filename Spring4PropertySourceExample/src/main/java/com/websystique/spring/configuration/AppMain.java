efcc8c9c31226cb513c4aaa9b19f3b2085392ba8
MyAsgnmnt <ankanghoshcs.1620@gmail.com>
2021-05-11 11:15:44 +0530

    Spring1

---
+++ Spring4PropertySourceExample/src/main/java/com/websystique/spring/configuration/AppMain.java
@@ -0,0 +1,20 @@
+package com.websystique.spring.configuration;
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+ 
+import com.websystique.spring.configuration.AppConfig;
+import com.websystique.spring.service.FileService;
+ 
+public class AppMain {
+     
+    public static void main(String args[]){
+        AbstractApplicationContext  context = new AnnotationConfigApplicationContext(AppConfig.class);
+        FileService service = (FileService) context.getBean("fileService");
+         
+        service.readValues();
+        context.close();
+    }
+     
+}
